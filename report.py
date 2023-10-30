b90ff80c3a363be31691891afd0cd87b701c7964
minhducnguyen26 <minh.nguyen@dmail.dixie.edu>
2023-10-29 23:15:58 -0600

    Modify report to generate html report

--- report.py
+++ report.py
@@ -1,6 +1,53 @@
 import os
 import subprocess
 
+def generate_html_report(passed, failed):
+    report = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+            body {{
+                font-family: Arial, sans-serif;
+            }}
+            .passed {{
+                color: green;
+            }}
+            .failed {{
+                color: red;
+            }}
+        </style>
+    </head>
+    <body>
+        <h1>Test Results</h1>
+        <p>Total tests executed: {len(passed) + len(failed)}</p>
+        <p class="passed">Tests passed: {len(passed)}</p>
+        <ul>
+    """
+
+    for path in passed:
+        report += f"            <li class='passed'>{path}</li>\n"
+
+    report += f"""
+        </ul>
+        <p class="failed">Tests failed: {len(failed)}</p>
+        <ul>
+    """
+
+    for path in failed:
+        report += f"            <li class='failed'>{path}</li>\n"
+
+    report += """
+        </ul>
+    </body>
+    </html>
+    """
+
+    with open("index.html", "w") as html_file:
+        html_file.write(report)
+
+    print(f"\nThe report is also updated to the index.html file.")
+
 def run_tests():
     GREEN = "\033[32m"
     RED = "\033[31m"
@@ -26,7 +73,6 @@ def run_tests():
 
                 total += 1
 
-
     print(f"\nRan all {total} test files and here's the result:")
     
     print(GREEN + f"\nPassed: {len(passed)}" + RESET)
@@ -37,6 +83,7 @@ def run_tests():
     for path in failed:
         print(f"    {path}")
 
+    generate_html_report(passed, failed)
 
 if __name__ == '__main__':
     run_tests()
